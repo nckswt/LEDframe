249c6f29bdec2c576fd8ca4936525063b2888229
Nick Sweet <nckswt@gmail.com>
2014-12-24 22:55:27 -0500

    christmas message

--- demo.sh
+++ demo.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 sleepline=0.5
-sleepframe=2
+sleepframe=2.5
 
+#Merry christmas mom & dad
 sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "Merry"
 sleep $sleepline
 echo "Christ-"
@@ -14,6 +15,7 @@ echo " "
 )
 sleep 1
 
+#Your suns made a small screen
 sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "Your"; sleep 1; echo "sons"
 sleep $sleepline
 echo "made"
@@ -26,6 +28,7 @@ echo " "
 )
 sleep 1
 
+#But it can do some cool things!
 sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "But it"
 sleep $sleepline
 echo "can do"
@@ -41,3 +44,187 @@ echo " "
 sleep 1
 
 sudo ./led-matrix -d -t 3 -D 0
+sleep 3.5
+
+#We're making it do cooler things
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "We're"
+sleep $sleepline
+echo "making"
+sleep $sleepline
+echo "it do"
+sleep $sleepline
+echo "cooler"
+sleep $sleepline
+echo "things"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#Like texting the screen directly
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "Like"
+sleep $sleepline
+echo "texting"
+sleep $sleepline
+echo "the"
+sleep $sleepline
+echo "screen"
+sleep $sleepline
+echo "directly"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#So we can send you thoughts from near and far
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "To send"
+sleep $sleepline
+echo "thoughts"
+sleep $sleepline
+echo "from"
+sleep $sleepline
+echo "near"
+sleep $sleepline
+echo "and far"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#We're also putting a webpage together
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "We're"
+sleep $sleepline
+echo "also"
+sleep $sleepline
+echo "making"
+sleep $sleepline
+echo "a small"
+sleep $sleepline
+echo "webpage"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#So you can play with it too!
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "So you"
+sleep $sleepline
+echo "can play"
+sleep $sleepline
+echo "with it"
+sleep $sleepline
+echo "too!"
+sleep $sleepline
+echo " "
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#And if there's anything that connects us, it's
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "And if"
+sleep $sleepline
+echo "anything"
+sleep $sleepline
+echo "connects"
+sleep $sleepline
+echo "us, it's"
+sleep $sleepline
+echo "..."
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+sudo ./led-matrix -d -t 3 -D 1 habs.ppm 
+sleep 3.5
+
+#So we're working on a scoreboard too
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "So we're"
+sleep $sleepline
+echo "making a"
+sleep $sleepline
+echo "score-"
+sleep $sleepline
+echo "board"
+sleep $sleepline
+echo "too"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#We had fun working on it together!
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "We had"
+sleep $sleepline
+echo "tons of"
+sleep $sleepline
+echo "fun"
+sleep $sleepline
+echo "working"
+sleep $sleepline
+echo "together"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#We're going to keep working at it
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "And we"
+sleep $sleepline
+echo "want to"
+sleep $sleepline
+echo "keep"
+sleep $sleepline
+echo "working"
+sleep $sleepline
+echo "on it!"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#But we want suggestions, too!
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "But, we"
+sleep $sleepline
+echo "want"
+sleep $sleepline
+echo "your"
+sleep $sleepline
+echo "ideas"
+sleep $sleepline
+echo "too!"
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#We hope you like it =)
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "We hope"
+sleep $sleepline
+echo "you like"
+sleep $sleepline
+echo "it"
+sleep $sleepline
+echo "=)"
+sleep $sleepline
+echo " "
+sleep $sleepframe
+echo " "
+)
+sleep 1
+
+#Merry Christmas -Alec and Nick
+sudo ./text-example -f fonts/4x6.bdf -y0 -C0,255,0 < <(echo "Merry"
+sleep $sleepline
+echo "Christ-"
+sleep $sleepline
+echo "mas!"
+sleep $sleepline
+echo "Alec &"
+sleep $sleepline
+echo "Nick"
+sleep $sleepframe
+echo " "
+)
+sleep 1
